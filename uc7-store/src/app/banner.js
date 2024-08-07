d712e94388b8a34f2a6c463f516eae29caf5b821
Pedro <pedroarmiliato2006@gmail.com>
2024-08-07 16:38:59 -0300

    add banner

---
+++ uc7-store/src/app/banner.js
@@ -0,0 +1,10 @@
+import './globals.css';
+import styles from './banner.module.css';
+
+export default function Banner() {
+    return (<div className={styles.ban}>
+        <h1 className={styles.pin}>Bem-vindos a nossa Loja Medieval!</h1>
+        <p className={styles.text}>Venha conferir nossa loja.</p>
+        <button className={styles.btn}>Conferir</button>
+    </div>);
+}
