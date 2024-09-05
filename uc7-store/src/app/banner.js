d690f842d32a5c838735d5e64d2715ae6ef805ea
Pedro <pedroarmiliato2006@gmail.com>
2024-09-05 14:07:25 -0300

    Refactor layout

--- uc7-store/src/app/banner.js
+++ uc7-store/src/app/banner.js
@@ -1,12 +1,15 @@
 import "./globals.css";
 import styles from "./banner.module.css";
+import Link from "next/link";
 
 export default function Banner() {
   return (
     <div className={styles.ban}>
       <h1 className={styles.pin}>Bem-vindos a nossa Loja Medieval!</h1>
       <p className={styles.text}>Venha conferir nossa loja.</p>
-      <button className={styles.btn}>Conferir</button>
+      <button className={styles.btn}>
+        <Link href={"/lancamentos"}>Conferir</Link>
+      </button>
     </div>
   );
 }
